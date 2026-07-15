47bfe6575bcea049d8e46130157af2d40de72e2e
Saqib Shabib <saqibshabib56@gmail.com>
2026-07-15 12:50:52 +0500

    Welcome Message Generator, Print Statement, and "Hello World1"

---
+++ day1.py
@@ -0,0 +1,11 @@
+print("Hello,World!")
+print("the number is ",4)
+print("2 + 2 =",2 + 2)
+
+#tring f sting
+name = "saqib"
+print(f"Hello {name} welcome to python programming")
+
+#user input
+username = input("What's your name?")
+print(f"Hello, {username} Welcome to python programming")
