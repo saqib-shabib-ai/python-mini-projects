be98b2a17b184ea1c5c796a5867f29318a6f2d9f
Saqib Shabib <saqibshabib56@gmail.com>
2026-07-23 00:29:06 +0500

    Personlized Welcome Message

---
+++ day2 project.py
@@ -0,0 +1,16 @@
+#welcome massage genrator
+
+
+#step 1: ask user details
+
+
+name = input("what's your name? ")
+hobby = input("what's your hobby? ")
+
+
+ # step 2: genraten a personlized welcome massage
+print("\n---welcome massage---")
+print(f"Hello,{name}!")
+print(f"welcome to python programming")
+print(f"it's great to know you love {hobby}")
+print(f"get ready to build something amazing today")
